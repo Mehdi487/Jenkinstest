1f41603316c54b74e365896d2a075c498417ebd4
Mehdi487 <53235410+Mehdi487@users.noreply.github.com>
2023-10-09 19:54:05 +0100

    Update script1.py

--- script1.py
+++ script1.py
@@ -10,7 +10,7 @@ nr = InitNornir(config_file=my_config)
 
 
 def test_connect(task):
-    task.run(task=netmiko_send_command, command_string="show ip interfaces brief")
+    task.run(task=netmiko_send_command, command_string="show ip interface brief")
 
 
 result = nr.run(task=test_connect)
